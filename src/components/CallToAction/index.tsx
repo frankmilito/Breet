5a0705b84183976ebe072d8db10dedca41336bad
Franklin Obidike <Franklin@Obi-Franklin.local>
2022-08-24 14:46:01 +0100

    fixing typing errors

--- src/components/CallToAction/index.tsx
+++ src/components/CallToAction/index.tsx
@@ -3,32 +3,43 @@ import { Button, TextField } from '@mui/material'
 import subBackground from '../../assets/subBackground.svg'
 const useStyles = makeStyles((theme) => ({
     container: {
+        margin: '0 8em',
         background: `url(${subBackground})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        borderRadius: '1em',
+
     },
     wrapper: {
         textAlign: 'center',
-        padding: '3em 0'
+        padding: '3em 0',
     },
     title: {
-        fontSize: '36px',
+        fontSize: '48px',
         width: '70%',
         color: 'white',
-        lineHeight: '72px'
+        lineHeight: '72px',
+        margin: '0 auto',
+        marginBottom: '1em'
     },
     input: {
         background: '#fff',
         marginRight: '20px',
-        borderRadius: '2em'
+        borderRadius: '2em',
+        border: 'none',
+        width: '100%',
+        padding: '1em 2em'
     },
     cta: {
-        display: 'flex',
-        alignItems: 'center',
+        display: 'grid',
+        gridTemplateColumns: '2fr 1fr',
+        gap: '2em',
+        width: '80%',
+        margin: '0 auto'
     }
 }))
 
@@ -39,7 +50,7 @@ const CallToAction = () => {
             <div className={classes.wrapper}>
                 <h1 className={classes.title}>An enterprise template to ramp up your company website</h1>
                 <div className={classes.cta}>
-                    <TextField fullWidth type="text" className={classes.input} />
+                    <input type="text" className={classes.input} />
                     <Button color='secondary' variant='contained' className='button-contained' style={{
                         width: '400px !important'
                     }}>Start Now</Button>
